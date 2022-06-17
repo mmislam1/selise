fab8852c283007841769daaa3a8453215dfe4634
mmislam <mmislam272@gmail.com>
2022-06-17 11:22:04 +0600

    Select option done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Calender from './components/calender.js';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Calender/>
       </header>
     </div>
   );
--- src/components/calender.js
+++ src/components/calender.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
 
-const calender = () => {
+
+const Calender = (props) => {
+  const date = new Date();
+  const [month, setMonth] = useState(date.getMonth())
+  useEffect(() => { }, [month])
+
+  date.setDate(1)
+  date.setMonth(month)
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August", "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+let counta=-1;
+
+const list=months.map((month)=>{counta+=1;
+  let sel=''
+  if(counta===date.getMonth()){
+sel='selected';
+  }
+  return <option value={counta} >{month}</option>})
+
+
   return (
-    <div>calender</div>
+    <div><select value={date.getMonth()} className='selectMonth' name='selectMonth'>
+
+      {list}
+    </select></div>
   )
 }
 
-export default calender
+export default Calender
